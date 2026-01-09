b3512d243b23618ed378317f3dcefebe05536b9b
nico4565 <raiti361@gmail.com>
2026-01-09 14:50:58 +0100

    add logic to process commands only two commands for now one to exit  and a help commands that prints all commands descriptions

--- repl/repl.go
+++ repl/repl.go
@@ -20,9 +20,33 @@ func StartRepl() {
 		}
 
 		parsed := cleanInput(text)
-		fmt.Printf("Your command was: %v\n", parsed[0])
+		command, exists := getCommands()[parsed[0]]
+		if !exists {
+			fmt.Print("Unknown command")
+		}
 
+		err := command.callback()
+		if err != nil {
+			fmt.Print(err)
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	commands := getCommands()
+	for command := range commands {
+		fmt.Printf("%s: %s\n", command, commands[command].description)
+	}
+
+	return nil
 }
 
 func cleanInput(text string) []string {
@@ -41,4 +65,24 @@ func cleanInput(text string) []string {
 	}
 
 	return append(res, text)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		}, "help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
